30207d5169137016b5a8e1671230f8a10f02dc94
casierrav <casierrav@unal.edu.co>
2022-11-24 09:16:14 -0500

    Main file of the applicacion

---
+++ src/main/java/com/misiontic/unal/securityBackend/SecurityBackendApplication.java
@@ -0,0 +1,35 @@
+package com.misiontic.unal.securityBackend;
+
+/*
+ * Copyright: Copyright (c) 2022
+ *
+ * License
+ *
+ * Copyright (c) 2022 by Carlos Andres Sierra Virgüez.
+ * All rights reserved.
+ *
+ * This file is part of Academic #MisionTIC2022 Project Software.
+ *
+ * Academic #MisionTIC2022 Project is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Academic #MisionTIC2022 Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Academic #MisionTIC2022 Project.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBackendApplication.class, args);
+	}
+
+}
